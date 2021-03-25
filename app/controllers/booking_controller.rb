f990714ac7b89b5dfb4a46bf1c400c8f5dd9acce
Ahmed Ibrahim <Ahmed.Ibrahhim@Outlook.com>
2021-03-25 11:35:48 +0200

    Booking checkout

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,26 @@
+require 'base64'
+
+class BookingController < ApplicationController
+  def checkout
+    seat_type = params[:flight_level]
+    flight_id = params[:flight_selected]
+    commit = params[:commit]
+    @flight_info = Flight.find(flight_id)
+    if commit == "Order"
+      number_of_seats_at_this_flight = Flight.find(flight_id).aero_plan.seat_configurations.select("sum(number_of_rows * seats_in_rows)").group("seat_configurations.id").first&.sum
+      number_of_booked_seats = Flight.find(flight_id).passengers.count
+      @total_price = @flight_info.price_per_economy if seat_type == "Economy"
+      @total_price = @flight_info.price_per_business if seat_type == "Business"
+      @total_price = @flight_info.price_per_first if seat_type == "FirstClass"
+    elsif commit == "Confirm"
+      passenger = Passenger.create(name: params[:name], email: params[:email])
+      @flight_booking = PassengerBooking.create(flight: @flight_info , passenger: passenger, seat_type: seat_type, paid_price: params[:total_price])
+    end
+  end
+
+  def choose_your_seat
+    @flight = PassengerBooking.where(pnr: params[:pnr]).first.flight
+    @booking = PassengerBooking.where(pnr: params[:pnr]).first
+    @seats = @flight.aero_plan.seat_configurations.order('seat_type desc')
+  end
+end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,17 @@
 class FlightsController < ApplicationController
+  def index
+    @flight = Flight.new
+    @airports = AirPort.all.map { |airport| [airport.city, airport.id] }
+    @dates = Flight.order("date asc").all.map { |flight| [flight.date.strftime("%d/%m/%Y")] }.uniq
+    @passengers = [1, 2, 3, 4]
+    if !params[:flight].nil?
+      @from = params[:flight][:from_airport]
+      @to = params[:flight][:to_airport]
+      @date = params[:flight][:date]
+      @passengers_select = params[:flight][:passengers]
+      @flights = Flight.search(@from, @to, @date)
+      render "flights/search_result"
+    end
+  end
+
 end
