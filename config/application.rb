40015c35a9049727b6e25f2abba55feee064d526
Ahmed Ibrahim <Ahmed.Ibrahhim@Outlook.com>
2021-03-25 15:07:52 +0200

    Stop precombile

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module Bookly
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
